37be214736082cf1b9bfd079c43d8892ff5d4589
cyberbrest <cyberbrest@cyberbrest.com>
2012-03-31 10:37:29 +0300

    [Tool/Extractor] update to 4.3.3

--- src/tools/map_extractor/System.cpp
+++ src/tools/map_extractor/System.cpp
@@ -73,7 +73,7 @@ float CONF_float_to_int16_limit = 2048.0f;   // Max accuracy = val/65536
 float CONF_flat_height_delta_limit = 0.005f; // If max - min less this value - surface is flat
 float CONF_flat_liquid_delta_limit = 0.001f; // If max - min less this value - liquid surface is flat
 
-uint32 CONF_TargetBuild = 14545;              // 4.2.2.14545
+uint32 CONF_TargetBuild = 15354;              // 4.3.3.15354
 
 // List MPQ for extract maps from
 char const* CONF_mpq_list[]=
@@ -85,7 +85,7 @@ char const* CONF_mpq_list[]=
     "expansion3.MPQ",
 };
 
-uint32 const Builds[] = {13164, 13205, 13287, 13329, 13596, 13623, 13914, 14007, 14333, 14480, 14545, 15005, 15050, 15211, 0};
+uint32 const Builds[] = {13164, 13205, 13287, 13329, 13596, 13623, 13914, 14007, 14333, 14480, 14545, 15005, 15050, 15211, 15354, 0};
 #define LAST_DBC_IN_DATA_BUILD 13623    // after this build mpqs with dbc are back to locale folder
 
 char* const Locales[] = {"enGB", "enUS", "deDE", "esES", "frFR", "koKR", "zhCN", "zhTW", "enCN", "enTW", "esMX", "ruRU"};
